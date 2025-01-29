621338f813e845fba2ef20be2e9a57b80c00e09e
NamaDah <drkfedi@gmail.com>
2025-01-29 11:01:32 +0700

    middleware

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -22,11 +22,15 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        if($request->user()->role === 'admin'){
+            return redirect('admin/dashboard');
+        }
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
